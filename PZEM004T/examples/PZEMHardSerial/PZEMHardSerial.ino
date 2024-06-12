7613aa1f932f20897a199e677d9d1b9b8b8e7e14
execute1d <akmal.tangsel@gmail.com>
2024-06-12 17:32:54 +0700

    Final Commit

---
+++ PZEM004T/examples/PZEMHardSerial/PZEMHardSerial.ino
@@ -0,0 +1,110 @@
+/*
+Copyright (c) 2021 Jakub Mandula
+
+Example of using one PZEM module with Hardware Serial interface.
+================================================================
+
+If desired, a HardwareSerial handle can be passed to the constructor
+which will then be used for the communication with the module.
+
+Note that ESP32 HardwareSerial must also be provided with the RX and TX
+pins.
+
+*/
+
+#include <PZEM004Tv30.h>
+
+
+#if !defined(PZEM_RX_PIN) && !defined(PZEM_TX_PIN)
+#define PZEM_RX_PIN 16
+#define PZEM_TX_PIN 17
+#endif
+
+#if !defined(PZEM_SERIAL)
+#define PZEM_SERIAL Serial2
+#endif
+
+
+#if defined(ESP32)
+/*************************
+ *  ESP32 initialization
+ * ---------------------
+ * 
+ * The ESP32 HW Serial interface can be routed to any GPIO pin 
+ * Here we initialize the PZEM on Serial2 with RX/TX pins 16 and 17
+ */
+PZEM004Tv30 pzem(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN);
+#elif defined(ESP8266)
+/*************************
+ *  ESP8266 initialization
+ * ---------------------
+ * 
+ * Not all Arduino boards come with multiple HW Serial ports.
+ * Serial2 is for example available on the Arduino MEGA 2560 but not Arduino Uno!
+ * The ESP32 HW Serial interface can be routed to any GPIO pin 
+ * Here we initialize the PZEM on Serial2 with default pins
+ */
+//PZEM004Tv30 pzem(Serial1);
+#else
+/*************************
+ *  Arduino initialization
+ * ---------------------
+ * 
+ * Not all Arduino boards come with multiple HW Serial ports.
+ * Serial2 is for example available on the Arduino MEGA 2560 but not Arduino Uno!
+ * The ESP32 HW Serial interface can be routed to any GPIO pin 
+ * Here we initialize the PZEM on Serial2 with default pins
+ */
+PZEM004Tv30 pzem(PZEM_SERIAL);
+#endif
+
+void setup() {
+    // Debugging Serial port
+    Serial.begin(115200);
+
+    // Uncomment in order to reset the internal energy counter
+    // pzem.resetEnergy()
+}
+
+void loop() {
+    // Print the custom address of the PZEM
+    Serial.print("Custom Address:");
+    Serial.println(pzem.readAddress(), HEX);
+
+    // Read the data from the sensor
+    float voltage = pzem.voltage();
+    float current = pzem.current();
+    float power = pzem.power();
+    float energy = pzem.energy();
+    float frequency = pzem.frequency();
+    float pf = pzem.pf();
+
+    // Check if the data is valid
+    if(isnan(voltage)){
+        Serial.println("Error reading voltage");
+    } else if (isnan(current)) {
+        Serial.println("Error reading current");
+    } else if (isnan(power)) {
+        Serial.println("Error reading power");
+    } else if (isnan(energy)) {
+        Serial.println("Error reading energy");
+    } else if (isnan(frequency)) {
+        Serial.println("Error reading frequency");
+    } else if (isnan(pf)) {
+        Serial.println("Error reading power factor");
+    } else {
+
+        // Print the values to the Serial console
+        Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
+        Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
+        Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
+        Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
+        Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
+        Serial.print("PF: ");           Serial.println(pf);
+
+    }
+
+
+    Serial.println();
+    delay(2000);
+}
---
+++ main.ino
@@ -0,0 +1,190 @@
+
+
+  // Data Communication Final Project
+
+  // Author : execute1d
+
+  // Internet Engineering Technology Tribone 2023
+
+  // Politeknik Elektronika Negeri Surabaya © 2024
+
+
+#define BLYNK_TEMPLATE_ID "TMPL6_e4tGMH3"
+#define BLYNK_TEMPLATE_NAME "EnergyIOT"
+#define BLYNK_AUTH_TOKEN "AQTwdx4wQOEk40391gl3MVX_thG4iYFd"
+
+#include <ESP8266WiFi.h>
+#include <WiFiManager.h>
+#include <BlynkSimpleEsp8266.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <PZEM004Tv30.h>
+
+  // Replace with your Blynk Auth Token
+char auth[] = BLYNK_AUTH_TOKEN;
+
+PZEM004Tv30 pzem(13, 12); // D7 TX, D6 RX
+
+LiquidCrystal_I2C lcd(0x27, 16, 2); // SDA D2, SCL D1
+
+BlynkTimer timer;
+
+bool wifiConnected = false;
+
+byte fullBar[8] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111
+};
+
+void showLoadingAnimation() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  int p = 1;
+  lcd.print("Initializing...");
+  for (int j = 0; j <= 7;++j){
+    for (int i = 0; i <= 16; ++i) { // Adjust the loop count as needed
+      lcd.setCursor(i, 1);
+      lcd.write(byte(0));
+      delay(120); // Adjust the delay as needed
+      lcd.setCursor(i, 1);
+      lcd.print(" ");
+   }
+   if (p == 0){
+   lcd.setCursor(0, 0);
+   lcd.print("Initializing...");
+   p = 1;
+   } else{
+   lcd.setCursor(0, 0);
+   lcd.print("Starting Up....");
+   p = 0;
+   }
+   
+  }
+  lcd.setCursor(0, 0);
+  lcd.print("Waiting Response");  
+  lcd.setCursor(0, 1);
+  lcd.print("Check Wifi AP !");
+}
+
+void setup() {
+  // Start serial communication
+  Serial.begin(115200);
+  // Initialize the LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.createChar(0, fullBar);
+
+  // Start Loading Animation first, then WiFi check procedure
+
+  showLoadingAnimation();
+  // Initialize WiFi Manager  
+  WiFiManager wifiManager;
+  wifiManager.setConfigPortalTimeout(180); 
+
+  // Uncomment line below to reset WiFi credentials on startup.
+  //wifiManager.resetSettings();
+
+  // if WiFi is outage, it will start access point web portal, if it reach timeout, it will proceed.
+  
+  if (!wifiManager.autoConnect("IoT Energy Monitor v.1.2","tribone23")) {
+    Serial.println("Failed to connect to WiFi and hit timeout");
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Timeout/Canceled");
+    lcd.setCursor(0, 1);
+    lcd.print("Startup Offline!");
+    wifiConnected = false;
+    delay(5000);
+  } else {
+    // If you get here, you have connected to the WiFi
+    Serial.println("Connected to WiFi");
+    wifiConnected = true;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(WiFi.localIP());
+    lcd.setCursor(0, 1);
+    lcd.print("Startup Online !");
+    delay(5000);
+
+    Blynk.config(auth);
+    Blynk.connect();
+  }
+
+  // Assign timer to read sensor data and send it to Blynk
+  timer.setInterval(1000L, sendData);
+  lcd.clear();
+}
+
+void sendData() {
+  int voltage = pzem.voltage();
+  int current = pzem.current();
+  int power = pzem.power();
+  int energy = pzem.energy();
+
+  if (!isnan(voltage) && voltage <= 260 ) {
+    if (wifiConnected) Blynk.virtualWrite(V1, voltage);
+    lcd.setCursor(0, 0);
+    if(voltage <=9)
+    {lcd.print("V: " + String(voltage) + "    ");}
+    if(voltage >9 && voltage <=99)
+    {lcd.print("V: " + String(voltage) + "   ");}
+    if(voltage >99)
+    {lcd.print("V: " + String(voltage) + "  ");}
+  } else {
+    lcd.setCursor(0, 0);
+    lcd.print("V: N/A  ");
+    if (wifiConnected) Blynk.virtualWrite(V1, 0);
+  }
+
+  // Update current
+  if (!isnan(current) && current <= 100) {
+    if (wifiConnected) Blynk.virtualWrite(V2, current);
+    lcd.setCursor(0, 1);
+    if(current <=9)
+    {lcd.print("A: " + String(current) + "    ");}
+    if(current >9 && current <=99)
+    {lcd.print("A: " + String(current) + "   ");}
+    if(current >99)
+    {lcd.print("A: " + String(current) + "  ");}
+  } else {
+    lcd.setCursor(0, 1);
+    lcd.print("A: N/A  ");
+    if (wifiConnected) Blynk.virtualWrite(V2, 0);
+  }
+
+  // Update power
+  if (!isnan(power)&& power <= 26000) {
+    if (wifiConnected) Blynk.virtualWrite(V3, power);
+    lcd.setCursor(8, 0);
+    lcd.print("W: " + String(power) + "    ");
+  } else {
+    lcd.setCursor(8, 0);
+    lcd.print("W: N/A  ");
+    if (wifiConnected) Blynk.virtualWrite(V3, 0);
+  }
+
+  // Update energy
+  if (!isnan(energy) && energy <= 9999) {
+    if (wifiConnected) Blynk.virtualWrite(V4, energy);
+    lcd.setCursor(8, 1);
+    lcd.print("E: " + String(energy) + "   ");
+  } else {
+    lcd.setCursor(8, 1);
+    lcd.print("E: N/A  ");
+  }
+  yield();
+}
+
+void loop() {
+  if (wifiConnected && WiFi.status() == WL_CONNECTED) {
+    Blynk.run();
+  }
+  timer.run();
+  yield();
+}
